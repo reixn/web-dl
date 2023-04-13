5b939a247d8964215dd06c6ae6da49de6cef5366
Kyle Rogers <reixn@outlook.com>
2023-04-13 13:24:37 +0800

    Move cli manifest to seperate mod

--- zhihu-dl/src/bin/main/main.rs
+++ zhihu-dl/src/bin/main/main.rs
@@ -4,115 +4,20 @@
 use anyhow::Context;
 use clap::{FromArgMatches, Parser, Subcommand, ValueEnum};
 use indicatif::HumanDuration;
-use std::{
-    fs,
-    io::{self, Seek},
-    path::PathBuf,
-};
+use std::{fs, path::PathBuf};
 use termcolor::{BufferedStandardStream, Color};
 use zhihu_dl::{
-    driver::{manifest::Manifest, Driver},
+    driver::Driver,
     progress::{progress_bar::ProgressReporter, OtherJob, Reporter},
     store,
 };
 
 mod container;
 mod item;
+mod manifest;
 mod types;
 
-use container::ContainerCmd;
-use item::ItemCmd;
 use types::*;
-
-#[derive(Debug, Subcommand)]
-enum ManifestOper {
-    Apply {
-        #[arg(default_value = "manifest.ron")]
-        path: String,
-    },
-    Format {
-        #[arg(default_value = "manifest.ron")]
-        path: String,
-    },
-    Link {
-        #[arg(default_value = "manifest.ron")]
-        path: String,
-    },
-}
-impl ManifestOper {
-    async fn run(
-        self,
-        reporter: &ProgressReporter,
-        output: &mut Output,
-        driver: &mut Driver,
-    ) -> Result<(), anyhow::Error> {
-        match self {
-            Self::Format { path } => {
-                let mut file = fs::OpenOptions::new()
-                    .read(true)
-                    .write(true)
-                    .open(path.as_str())
-                    .with_context(|| format!("failed to open file {}", path))?;
-                let m: Manifest = ron::de::from_reader(io::BufReader::new(&file))
-                    .context("failed to deserialize ron")?;
-                file.set_len(0)
-                    .context("failed to truncate file for write")?;
-                file.rewind().context("failed to seek to begin")?;
-                ron::ser::to_writer_pretty(
-                    io::BufWriter::new(&file),
-                    &m,
-                    ron::ser::PrettyConfig::default(),
-                )
-                .context("failed to serialize ron")?;
-                output.write_tagged(
-                    Color::Green,
-                    "Formatted",
-                    format_args_nl!("ron manifest {}", path),
-                );
-            }
-            Self::Apply { path } => {
-                let job = reporter.start_job("Applying", format_args!("manifest {}", path));
-                driver
-                    .apply_manifest(
-                        reporter,
-                        &ron::de::from_reader(io::BufReader::new(
-                            fs::File::open(path.as_str())
-                                .with_context(|| format!("failed to open file {}", path))?,
-                        ))
-                        .context("failed to deserialize ron")?,
-                        std::env::current_dir().context("failed to get current directory")?,
-                    )
-                    .await
-                    .context("failed to apply manifest")?;
-                job.finish("Applied", format_args!("manifest {}", path,));
-            }
-            Self::Link { path } => {
-                let job = reporter.start_job(
-                    "Creating",
-                    format_args!("symbol links according to manifest {}", path),
-                );
-                driver
-                    .link_manifest(
-                        reporter,
-                        &ron::de::from_reader(io::BufReader::new(
-                            fs::File::open(&path)
-                                .with_context(|| format!("failed to open file {}", path))?,
-                        ))
-                        .context("failed to deserialize manifest")?,
-                        std::env::current_dir().context("failed to get current directory")?,
-                    )
-                    .with_context(|| {
-                        format!("failed to create symbol links according to {}", path)
-                    })?;
-                job.finish(
-                    "Created",
-                    format_args!("symbol links according to {}", path),
-                );
-            }
-        }
-        Ok(())
-    }
-}
 
 #[derive(Debug, Subcommand)]
 enum Command {
@@ -120,18 +25,18 @@ enum Command {
     Init,
     Item {
         #[command(subcommand)]
-        cmd: ItemCmd,
+        cmd: item::ItemCmd,
     },
     Container {
         #[command(subcommand)]
-        cmd: ContainerCmd,
+        cmd: container::ContainerCmd,
     },
     Command {
         file: String,
     },
     Manifest {
         #[command(subcommand)]
-        operation: ManifestOper,
+        operation: manifest::ManifestCmd,
     },
     /// migrate store
     Migrate,
